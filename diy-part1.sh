99681c6a69119051f08981eadcfdd71c13cead5e
HeroYu920 <52651756+HeroYu920@users.noreply.github.com>
2022-01-27 16:23:16 +0800

    Update diy-part1.sh
    add openclash

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,3 +26,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 #Update jerrykuku argon theme for lean openwrt
 rm -rf package/lean/luci-theme-argon/
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+#Add openclash
+git clone https://github.com/vernesong/OpenClash.git ~
+mv ~/OpenClash/luci-app-openclash ~/openwrt/package/lean
