fb00873aa9cb8426155c7c11aab8942172cccac0
HeroYu920 <52651756+HeroYu920@users.noreply.github.com>
2022-02-25 21:58:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,23 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 #Modify default gateway and netmask address
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.240.0/g' package/base-files/files/bin/config_generate
 
 # Add a feed source for lean openwrt
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# Add a feed source for lienol openwrt
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-#Update jerrykuku argon theme for lean openwrt
-rm -rf package/lean/luci-theme-argon/
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-#Add openclash
-git clone https://github.com/vernesong/OpenClash.git ~
-mv ~/OpenClash/luci-app-openclash ~/openwrt/package/lean
