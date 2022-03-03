cb04f323225a367fb5fef29aefc049a64e8394aa
HeroYu920 <52651756+HeroYu920@users.noreply.github.com>
2022-03-03 22:21:19 +0800

    Update diy-part1.sh
    For Zoom's R7800

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,11 @@
 #
 
 #Modify default gateway and netmask address
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-sed -i 's/255.255.255.0/255.255.240.0/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/255.255.255.0/255.255.240.0/g' package/base-files/files/bin/config_generate
+
+#For Zoom's R7800
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Add a feed source for lean openwrt
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
